b4a2fa3c599349d49ab0070153d4ea2855fa117d
kristoffer johansson <kristoffer.c.johansson@gmail.com>
2021-12-15 16:27:36 +0100

    Update config.h

--- config.h
+++ config.h
@@ -46,12 +46,9 @@
 //#define LED_COMPOSE_PIN B3
 //#define LED_KANA_PIN B4
 
-//#define BACKLIGHT_PIN B7
-//#define BACKLIGHT_LEVELS 3
-//#define BACKLIGHT_BREATHING
 #define RGB_DI_PIN D5
-//#ifdef RGB_DI_PIN
-# define RGBLED_NUM 1
+// #define RGBLED_NUM 1
+#define RGBLED_SPLIT 2
 //#define RGB_DI_PIN E2
 //#ifdef RGB_DI_PIN
 //#    define RGBLED_NUM 16
