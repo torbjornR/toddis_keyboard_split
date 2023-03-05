45594358e2fd1c5a2a9d19967b8b722d8ad26dc9
torbjorn rasmusson <torbjorn.rasmusson@gmail.com>
2023-03-05 09:47:08 +0100

    first try of qwerty

--- keymaps/toddis1/keymap.c
+++ keymaps/toddis1/keymap.c
@@ -55,16 +55,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
     // left hand
     
-    KC_TAB,   KC_QUOT,   	KC_COMM,      	KC_DOT, 		KC_P, 	KC_Y,
-    KC_ESC,   KC_A,           	KC_O, 		KC_E,   		KC_U, 	KC_I,
-    KC_LSFT,  KC_SCOLON,      	KC_Q,         	KC_J,   		KC_K, 	KC_X,
+    KC_TAB,   KC_Q,   	KC_W,			KC_E,			KC_R, 	KC_T,
+    KC_ESC,   KC_A,     KC_S,			KC_D,   		KC_F, 	KC_G,
+    KC_LSFT,  KC_Z,     KC_X,         	KC_C,   		KC_V, 	KC_B,
     			      	KC_LALT,      	TD(TD_LCTRL_SUP), 	LT(_LAY0, KC_HOME),
 
     // right hand 
-    KC_F,  KC_G, KC_C, KC_R, KC_L, KC_SLSH,
-    KC_D,  KC_H, KC_T, KC_N, KC_S, KC_MINS,
-    KC_B,  KC_M, KC_W, KC_V, KC_Z, TD(TD_RSFT_DEL),   
-    LT(_LAY1, KC_ENT), KC_SPC, KC_BSPC
+    KC_Y,  KC_U,		KC_I,		KC_O,	KC_P,		KC_SLSH,
+    KC_H,  KC_J,		KC_K,		KC_L,	KC_SCLN,	KC_QUOT,
+    KC_N,  KC_M,		KC_COMM,	KC_DOT, KC_SLSH,	TD(TD_RSFT_DEL),   
+    LT(_LAY1, KC_ENT),	KC_SPC, KC_BSPC
 
     ),
     [_LAY0] = LAYOUT(
